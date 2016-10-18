76c397ac7914828349d24d700da1ea4932cf2c8c
tjackows <tjackows@uncc.edu>
2016-10-18 03:38:31 +0000

    Add css stylesheet

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,8 @@ class MoviesController < ApplicationController
   @movie = Movie.find(id) # look up movie by unique ID
   # will render app/views/movies/show.html.haml by default
   end
+  def new
+  @movie = Movie.new
+  # default: render 'new' template
+end 
 end
