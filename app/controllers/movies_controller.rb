a8a510b09134d33da04f6e65cd1d261d80bd61b1
tjackows <tjackows@uncc.edu>
2016-10-25 13:41:23 +0000

    Add edit/update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,6 +18,15 @@ def create
  flash[:notice] = "#{@movie.title} was successfully created."
  redirect_to movies_path
 end
+def edit
+    @movie = Movie.find params[:id]
+end
+def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(user_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movies_path(@movie)
+end
 private
 def user_params
  params.require(:movie)
