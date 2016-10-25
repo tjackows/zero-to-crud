3671863ceb6a3e51759fd3f7b4db28148227c607
tjackows <tjackows@uncc.edu>
2016-10-25 14:05:31 +0000

    Add destroy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,9 +27,16 @@ def update
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movies_path(@movie)
 end
+def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+end
 private
 def user_params
  params.require(:movie)
  params[:movie].permit(:title,:rating,:release_date)
 end
+
 end
